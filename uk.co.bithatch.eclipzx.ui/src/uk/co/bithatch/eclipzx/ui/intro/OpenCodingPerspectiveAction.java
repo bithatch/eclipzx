0102ca31db948332d638e2d5ab379a5d1409e1fc
Brett Smith <tanktarta@gmail.com>
2026-06-06 13:06:41 +0100

    Short intro on perspectives in the welcome page.

---
+++ uk.co.bithatch.eclipzx.ui/src/uk/co/bithatch/eclipzx/ui/intro/OpenCodingPerspectiveAction.java
@@ -0,0 +1,19 @@
+package uk.co.bithatch.eclipzx.ui.intro;
+
+import java.util.Properties;
+
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.intro.IIntroSite;
+import org.eclipse.ui.intro.config.IIntroAction;
+
+import uk.co.bithatch.bitzx.ZXPerspectives;
+import uk.co.bithatch.widgetzx.ZXPerspectivesUI;
+
+public class OpenCodingPerspectiveAction implements IIntroAction {
+    @Override
+    public final void run(IIntroSite site, Properties params) {
+    	ZXPerspectivesUI.switchToPerspective(ZXPerspectives.ZX_CODING_ID, PlatformUI.getWorkbench().getActiveWorkbenchWindow());
+    }
+
+	
+}
